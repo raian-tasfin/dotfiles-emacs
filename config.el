c67f71c5b2edbe4b12b93d18f3512a873d94e389
raian-tasfin <raian.tasfin-prog@yandex.com>
2024-01-05 23:08:17 +0600

    lightweight-emacs running well now

---
+++ config.el
@@ -0,0 +1,50 @@
+(add-hook 'org-mode-hook #'org-num-mode)
+
+(setq-default c-basic-offset 4)
+
+(setq auto-save-file-name-transforms
+  `((".*" ,temporary-file-directory t)))
+
+(setq inhibit-startup-message t)
+
+(tool-bar-mode -1)
+
+(menu-bar-mode -1)
+
+(set-frame-font "Fira Code-11.5" nil t)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+(use-package modus-themes
+  :config
+  (load-theme 'modus-operandi-tinted t))
+
+(use-package spaceline
+  :config
+  (spaceline-spacemacs-theme)
+  (spaceline-toggle-minor-modes-off)
+  (spaceline-toggle-buffer-encoding-off)
+  (spaceline-toggle-buffer-encoding-abbrev-off)
+  (setq spaceline-highlight-face-func 
+    'spaceline-highlight-face-evil-state)
+  (spaceline-define-segment line-column
+    "The current line and column numbers."
+    "l:%l c:%2c")
+  (spaceline-define-segment time
+    "The current time."
+    (format-time-string "%H:%M"))
+  (spaceline-define-segment date
+    "The current date."
+    (format-time-string "%h %d"))
+  (spaceline-toggle-time-on)
+  (spaceline-emacs-theme 'date 'time))
+
+(setq default-tab-width 4)
+
+(setq-default auto-fill-function 'do-auto-fill)
+
+(electric-pair-mode 1)
+
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/") t)
+
+;; include manual-package directories
+(let ((default-directory  "~/.emacs.d/manual-packages/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; we need use-package for the following
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; load org config
+(org-babel-load-file "~/.emacs.d/config.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(spaceline modus-themes yasnippet-snippets cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
